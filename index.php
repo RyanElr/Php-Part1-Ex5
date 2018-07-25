5535ec1e0b1ba77c68dc0c212c4512dfdab9f66a
Ryan <ryan.elr@outlook.com>
2018-07-25 14:28:36 +0200

    ex5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice5 Part1</title>
+  </head>
+  <body>
+    <?php
+    $number = NULL;
+    echo $number.'<br/>';
+    $number = 11;
+    echo $number.'<br/>';
+    ?>
+  </body>
+</html>
